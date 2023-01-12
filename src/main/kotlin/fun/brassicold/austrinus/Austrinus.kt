eeba055e0cb845d1c6fe7f33429e418b0a69243b
Morrigan226 <2667696596@qq.com>
2023-01-12 15:35:41 +0800

    第一次提交

---
+++ src/main/kotlin/fun/brassicold/austrinus/Austrinus.kt
@@ -0,0 +1,11 @@
+package fun.brassicold.austrinus
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object Austrinus : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
